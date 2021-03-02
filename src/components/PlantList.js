7ff04dcb3e3aaca141fcd17c258033b7b86532cd
Lindell Carter <lindellcarternyc@gmail.com>
2021-03-02 18:28:02 -0500

    create Delete modal and use in app; dummy delete plant callback

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
+import DeleteModal from './DeleteModal';
+
 // dummy data for testing display, delete when get from backend is implemented
 const dummyList = [
   {
@@ -25,24 +27,47 @@ const dummyList = [
 
 export default function PlantList(props) {
   const { plantList , deletePlant } = props;
+  console.log('plantlist props', props)
   // change useState to initialize plantList when connected to backend
   const [plants, setPlants] = useState(dummyList)
+  const [toDelete, setToDelete] = useState(null)
+
+  const onClickDelete = ({ id, nickname }) => {
+    console.log('onClickDelete', id, nickname)
+    setToDelete({ id, nickname })
+  }
+
+  const confirmDelete = () => {
+    if (toDelete) {
+      deletePlant(toDelete.id)
+      setToDelete(null)
+    }
+  }
+
+  const cancelDelete = () => {
+    setToDelete(null)
+  }
 
   return (
     <div className="plant-list-wrapper">
+      <DeleteModal 
+        isOpen={!!toDelete}
+        confirm={confirmDelete}
+        cancel={cancelDelete}
+        text={toDelete ? `Delete ${toDelete.nickname}` : 'Delete this plant?'}
+
+      />
       {plants.map(plant => (
-        <Link to={`/plant-list/${plant.id}`}>
-          <div className="plant-card" key={plant.id}>
-            <h3>{plant.nickname}</h3>
-            <p>{plant.h2oFrequency}</p>
-            <button 
-              className='delete-button'
-            //   onClick={deletePlant(plant.id)}
-              >Delete
-            </button>
-            {/* Delete button needs functionality...modal window to confirm?? */}
-          </div>
-        </Link>
+        <div className="plant-card" key={plant.id}>
+          <Link to={`/plantlist/${plant.id}`}><h3>{plant.nickname}</h3></Link>
+          <p>{plant.h2oFrequency}</p>
+          <button 
+            className='delete-button'
+            onClick={_ => onClickDelete({ id: plant.id, nickname: plant.nickname})}
+            >Delete
+          </button>
+          {/* Delete button needs functionality...modal window to confirm?? */}
+        </div>
       ))}
     </div>
   )
