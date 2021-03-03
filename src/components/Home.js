19047a1e6cb2b1fb1becfcc96823d29ef490f9ea
Rebecca Peckman <rebeccapeckman@gmail.com>
2021-03-03 22:35:21 +0000

    a

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ const StyledHome = styled.div`
     
     display: flex;
     flex-direction: column;
-    justify-content: space-between;
+    justify-content: center;
     align-items: center;
 
     #logo {
