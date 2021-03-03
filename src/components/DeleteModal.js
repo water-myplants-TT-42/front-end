3ad84f275995d4e83aad6e158cf45472fa0cfddf
Lindell Carter <lindellcarternyc@gmail.com>
2021-03-03 14:54:59 -0500

    update delete modal styles

--- src/components/DeleteModal.js
+++ src/components/DeleteModal.js
@@ -1,3 +1,39 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import Button from './styled/Button'
+
+const DeleteModalWrapper = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(242, 242, 242, 0.8);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    & > div {
+        background-color: white;
+        text-align: center;
+        padding: 1rem;
+        width: 100%;
+        max-width: 400px;
+
+
+        h4 {
+            margin-bottom: 2rem;
+            font-size: 2rem;
+        }
+
+        & > div {
+            display: flex;
+            justify-content: space-between;
+        }
+    }
+`
+
 export default function DeleteModal(props) {
     const { isOpen, text, confirm, cancel } = props
 
@@ -15,28 +51,17 @@ export default function DeleteModal(props) {
     }
 
     return (
-        <div style={{
-            position: 'fixed',
-            top: 0, left: 0,
-            bottom: 0, right: 0,
-            backgroundColor: `rgba(100, 100, 100, 0.7)`,
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center'
-
-        }}
+        <DeleteModalWrapper
             onClick={handleClick}
             id="delete-modal"
         >
-            <div style={{
-                background: 'white',
-                textAlign: 'center',
-                padding: '1rem'
-            }}>
+            <div>
                 <h4>{text}</h4>
-                <button id="confirm-delete" onClick={handleClick}>Confirm</button>
-                <button id="cancel-delete" onClick={handleClick}>Cancel</button>
+                <div>
+                    <Button size="small" variant="success" id="cancel-delete" onClick={handleClick}>Cancel</Button>
+                    <Button size="small" variant="danger" id="confirm-delete" onClick={handleClick}>Confirm</Button>
+                </div>
             </div>
-        </div>
+        </DeleteModalWrapper>
     )
 }
