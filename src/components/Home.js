4552e38fc2b173df43c5a8a9f631073aeb6094df
Rebecca Peckman <rebeccapeckman@gmail.com>
2021-03-03 21:13:42 +0000

    Home component mobile styled

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,35 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom'
+import styled from 'styled-components'
+
+import logo from '../icons/plant-color.svg'
+import Button from './styled/Button'
+
+const StyledHome = styled.div`
+    
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+
+    #logo {
+        width: 50%;
+        height: auto;
+        margin-bottom: 20px;
+    }
+
+    h1 {
+        font-size: 36px;
+        margin-bottom: 20px;
+    }
+
+    button {
+        width: 200px;
+        height: 50px;
+        font-size: 24px;
+        margin-bottom: 20px;
+    }
+`
 
 export default function Home(props) {
     const history = useHistory()
@@ -9,11 +39,19 @@ export default function Home(props) {
     }
 
     return (
-        <div>
-            {/* Logo here */}
+        <StyledHome>
+            <img src={logo} id='logo'/>
             <h1>Water My Plants</h1>
-            <button onClick={() => routeTo('/login')}>Log In</button>
-            <button onClick={() => routeTo('/signup')}>Sign Up</button>
-        </div>
+            <Button 
+                onClick={() => routeTo('/login')} 
+                variant='success'
+                children='Log In'
+            />
+            <Button 
+                onClick={() => routeTo('/signup')} 
+                variant='success'
+                children='Sign Up'
+            />
+        </StyledHome>
     )
 }
