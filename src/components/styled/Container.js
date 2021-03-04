289b7e8d9d7eec9034e7743ef207d07291ca21f7
Rebecca Peckman <rebeccapeckman@gmail.com>
2021-03-04 23:12:16 +0000

    reverted Container to initial commit

--- src/components/styled/Container.js
+++ src/components/styled/Container.js
@@ -5,16 +5,9 @@ const ContainerWrapper = styled.div`
     margin: 0 auto;
     width: 100%;
     max-width: ${(props) => props.maxWidth};
-
     display: flex;
     flex-direction: column;
-    justify-content: space-around;
     align-items: center;
-
-    h1 {
-        font-size: ${props => props.theme.h1FontSize};
-        margin-bottom: ${props => props.theme.space};
-    }
 `
 
 export default function Container(props) {
@@ -25,4 +18,4 @@ export default function Container(props) {
             {children}
         </ContainerWrapper>
     )
-}
+} 
