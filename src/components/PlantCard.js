623822ad871296f049fa039a7f03af06f6ffe38c
Lindell Carter <lindellcarternyc@gmail.com>
2021-03-04 17:50:49 -0500

    update to use Image component

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,31 +1,36 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
-import plantImage from '../images/plant-image.png'
 
 import Button, { ButtonWrapper } from './styled/Button'
+import Image, { ImageWrapper } from './styled/Image'
 
 const CardWrapper = styled.div`
+    z-index: 1;
     position: relative;
-    background-image: ${props => `url(${props.image})`};
-    background-size: cover;
     width: ${props => props.theme.cardSizeSmall};
     height: ${props => props.theme.cardSizeSmall};
     margin: ${props => props.theme.space};
     border: ${props => props.theme.cardBorder};
     box-shadow: ${props => props.theme.cardBoxShadow};
 
+    ${ImageWrapper} {
+        position: absolute;
+        width: ${props => props.theme.cardSizeSmall};
+        height: ${props => props.theme.cardSizeSmall};
+        z-index: -1;
+        left: -1px;
+    }
+
     @media only screen and (min-width: 768px) {
         width: ${props => props.theme.cardSizeLarge};
         height: ${props => props.theme.cardSizeLarge};
-    }
 
-    ${ButtonWrapper} {
-        position: absolute;
-        bottom: 1rem;
-        left: 50%;
-        transform: translateX(-50%);
-        margin: 0;
+        ${ImageWrapper} {
+            position: absolute;
+            width: ${props => props.theme.cardSizeLarge};
+            height: ${props => props.theme.cardSizeLarge};
+        }
     }
 `
 
@@ -50,17 +55,30 @@ const CardTitle = styled.h5`
     }
 `
 
+const CardContent = styled.div`
+    ${ButtonWrapper} {
+        position: absolute;
+        bottom: 1rem;
+        left: 50%;
+        transform: translateX(-50%);
+        margin: 0;
+    }
+`
+
 export default function Card(props) {
     const { plant, deletePlant } = props
 
     return (
-        <CardWrapper image={plant.image || plantImage}>
-            <CardTitle>
-                <Link to={`/plantlist/${plant.plant_id}`}>
-                    <p>{plant.nickname}</p>
-                </Link>
-            </CardTitle>
-            <Button size="mini" variant="danger" onClick={() => deletePlant(plant)}>Delete</Button>
+        <CardWrapper>
+            <Image src={plant.image}/>
+            <CardContent>
+                <CardTitle>
+                    <Link to={`/plantlist/${plant.plant_id}`}>
+                        <p>{plant.nickname}</p>
+                    </Link>
+                </CardTitle>
+                <Button size="mini" variant="danger" onClick={() => deletePlant(plant)}>Delete</Button>
+            </CardContent>
         </CardWrapper>
     )
 }
